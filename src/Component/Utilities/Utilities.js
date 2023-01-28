419db4119bf7ece4f5b98b55a47334030c022f4c
Md Naimur Zaman Sunny <egmdsunny@gmail.com>
2023-01-28 23:43:31 +0600

    Modify

--- src/Component/Utilities/Utilities.js
+++ src/Component/Utilities/Utilities.js
@@ -1,11 +1,24 @@
 const addDb = (id) =>{
-    const quantity = localStorage.getItem(id);
-    if(quantity){
-       const newQuantity = parseInt(quantity) + 1;
-       localStorage.setItem(id,newQuantity);
+    let shoppingCart = {};
+   
+    const storedCard = localStorage.getItem('shopping-cart');
+    if(storedCard){
+        shoppingCart = JSON.parse(storedCard)
     }else{
-        localStorage.setItem(id,1)
+        shoppingCart = {};
     }
+
+    // const quantity = localStorage.getItem(id);
+    const quantity = shoppingCart[id]
+    if(quantity){
+       const newQuantity = quantity + 1;
+       shoppingCart[id] = newQuantity;
+    //    localStorage.setItem(id,newQuantity);
+    }else{
+        shoppingCart[id] = 1;
+        // localStorage.setItem(id,1)
+    }
+    localStorage.setItem('shopping-cart',JSON.stringify(shoppingCart))
 }
 export { addDb };
 
